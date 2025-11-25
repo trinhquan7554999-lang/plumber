27e2f0e59843a82ad8d91e01fc568b36996145f7
trinhquan7554999-lang <trinhquan7554999@gmail.com>
2025-11-25 17:05:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve the files in the "public" folder (our index.html)
-app.use(express.static(path.join(__dirname, "public")));
+// Serve all static files (html, css, img, etc.) from the repo root
+app.use(express.static(__dirname));
+
+// Home page: send index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
